60fc71f1db937c3ed63f4b0fe334ba911f670f81
geekkid1 <captain.emily.blum@gmail.com>
2025-04-24 15:15:25 -0400

    Remove debug and improve redirect for filter endpoint

--- provider-view/src/main/java/com/example/demo/ProviderController.java
+++ provider-view/src/main/java/com/example/demo/ProviderController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,7 +24,6 @@ public class ProviderController {
     public String table(Model model, @PathVariable String product) {
         FeedbackData[] all = dbs.getFbByProduct(product);
         DummyFilterClass dfc = dbs.getFilter(product);
-        System.out.println(product);
         ArrayList<Long> updated = (ArrayList<Long>) urs.getUpdated();
         ArrayList<HashMap<String,Object>> data = new ArrayList<>();
         for (FeedbackData fd : all) {
@@ -44,9 +44,10 @@ public class ProviderController {
     }
 
     @PostMapping("/filter")
-    public String filter(@ModelAttribute DummyFilterClass filter, Model model) {
+    public ModelAndView filter(@ModelAttribute DummyFilterClass filter, Model model) {
         model.addAttribute("path", "table/" + filter.product);
         dbs.updateFilter(filter);
-        return table(model, filter.product);
+        //return table(model, filter.product);
+        return new ModelAndView("redirect:table/"+filter.product);
     }
 }
